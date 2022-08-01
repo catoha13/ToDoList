9f2225519a37034d6f8f3c864b5e3db6a78d3e31
Artom Prishchepov <catoha@gmail.com>
2022-08-01 22:39:32 +0300

    Move to AddTask folder

---
+++ ToDoList/Views/MainScreen/Create/Add Task/CreateTaskView.swift
@@ -0,0 +1,194 @@
+import SwiftUI
+
+struct CreateTaskView: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    @State private var assignee = ""
+    @State private var project = ""
+    @State private var title = ""
+    @State private var description = ""
+    @State private var getDate = "Anytime"
+    @State private var addTaskPressed = false
+    @State private var showSideView = false
+    @State private var searchedUser = ""
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                //MARK: Header
+                Header(text: "New Task")
+                ZStack {
+                    //MARK: Bottom View
+                    Bottom()
+                    //MARK: View
+                    VStack(alignment: .center) {
+                        HStack {
+                            //MARK: Assignee
+                            TextAndTextfield(text: $assignee, description: "Assignee")
+                            Spacer()
+                            //MARK: Project
+                            TextAndTextfield(text: $project, description: "Project")
+                        }
+                        .padding(.top, -10)
+                        .padding(.bottom, 10)
+                        .padding(.horizontal)
+                        
+                        //MARK: Title
+                        TextField(text: $title) {
+                            Text("Title")
+                        }
+                        .padding()
+                        .font(Font(Roboto.thinItalic(size: 18)))
+                        .frame(height: 66)
+                        .background(.bar)
+                        
+                        //MARK: Description Label
+                        HStack {
+                            Text("Description")
+                                .font(Font(Roboto.regular(size: 16)))
+                                .foregroundColor(.secondary)
+                            Spacer()
+                        }
+                        .padding(.top)
+                        .padding(.leading, 24)
+                        
+                        //MARK: Description Text
+                        VStack {
+                            TextField(text: $description) {
+                                Text("Text here")
+                                    .font(Font(Roboto.regular(size: 16)))
+                            }
+                            .padding(.horizontal, 10)
+                            Spacer()
+                            HStack {
+                                //MARK: Attachments
+                                Button {
+                                    
+                                } label: {
+                                    Image(systemName: "paperclip")
+                                        .resizable()
+                                        .frame(width: 19.61, height: 20.54)
+                                        .foregroundColor(.secondary)
+                                        .padding(.all, 10)
+                                    
+                                }
+                                Spacer()
+                            }
+                            .background(.bar)
+                        }
+                        .frame(width: 295, height: 120)
+                        .overlay(RoundedRectangle(cornerRadius: Constants.radiusFive)
+                            .stroke(lineWidth: 1)
+                            .foregroundColor(.customGray))
+                        .cornerRadius(Constants.radiusFive)
+                        
+                        HStack {
+                            Text("Due Date")
+                            Button {
+                                
+                            } label: {
+                                Text(getDate)
+                            }
+                            .frame(width: 90, height: 32)
+                            .background(Color.customBlue)
+                            .foregroundColor(.white)
+                            .cornerRadius(Constants.radiusFive)
+                            Spacer()
+                            
+                        }
+                        .padding(.leading, 24)
+                        .frame(height: 66)
+                        .background(.bar)
+                        .padding(.vertical)
+                        
+                        //MARK: Add Member
+                        VStack {
+                            HStack {
+                                Text("Add Member")
+                                    .font(Font(Roboto.regular(size: 16)))
+                                Spacer()
+                            }
+                            .padding(.horizontal)
+                            HStack {
+                                Text("Anyone")
+                                    .frame(width: 90, height: 58)
+                                    .multilineTextAlignment(.center)
+                                    .background(.bar)
+                                    .cornerRadius(Constants.raidiusFifty)
+                                Button {
+                                    
+                                } label: {
+                                    Text("+")
+                                        .frame(width: 32, height: 32)
+                                        .font(Font(Roboto.regular(size: 16)))
+                                        .background(Color.secondary)
+                                        .foregroundColor(.white)
+                                        .clipShape(Circle())
+                                    
+                                }
+                                Spacer()
+                            }
+                        }
+                        .padding(.top, 20)
+                        .padding(.bottom, 30)
+                        .padding(.leading, 10)
+                        
+                        //MARK: Custom Button
+                        CustomCoralFilledButton(text: "Add Task") {
+                            withAnimation {
+                                addTaskPressed.toggle()
+                            }
+                        }
+                    }
+                    .frame(width: 343, height: 669)
+                    .background(.white)
+                    .cornerRadius(Constants.radiusFive)
+                    .offset(y: -40)
+                    .shadow(radius: 4)
+                    
+                    if assignee != searchedUser {
+                        //MARK: SearchUser View
+                        SearchUserView(filteredText: $assignee,searchedUser: $assignee) {
+                            searchedUser = assignee
+                        }
+                    }
+                }
+            }
+            .ignoresSafeArea()
+            
+            //MARK: Show ViewTask
+            if addTaskPressed {
+                ViewTask(title: title,
+                         image: "pathFirst",
+                         username: assignee,
+                         dueDate: "Jul 13, 2022",
+                         description: description,
+                         tag: project,
+                         color: .customBlue,
+                         showSideView: $showSideView,
+                         closeViewTask: $addTaskPressed)
+                .offset(y: -20)
+            }
+            //MARK: Show SideView
+            if showSideView {
+                SideView(isPresented: $showSideView,
+                         firstText: "Add Member",
+                         secondText: "Edit Task",
+                         thirdText: "Delete Task")
+                .onTapGesture {
+                    if showSideView {
+                        showSideView.toggle()
+                    }
+                }
+                
+            }
+        }
+        .navigationBarHidden(true)
+    }
+}
+
+struct CreateTaskView_Previews: PreviewProvider {
+    @State static var isPresented = false
+    static var previews: some View {
+        CreateTaskView()
+    }
+}
---
+++ ToDoList/Views/MainScreen/Create/Add Task/ViewTask.swift
@@ -0,0 +1,227 @@
+import SwiftUI
+
+struct ViewTask: View {
+    //MARK: Assigned
+    @State var title = "Meeting according with design team in Central Park"
+    @State var image = "superhero"
+    @State var username = "Stephen Chow"
+    //MARK: Due
+    @State var dueDate = "Aug 4, 2020"
+    @State var description = "Lorem ipsum dolor sit amet, consectetur adipiscing. "
+    //MARK: Tag
+    @State var tag = "Personal"
+    @State var color: Color = .customBlue
+    //MARK: comment
+    @State private var showComments = false
+    @Binding var showSideView: Bool
+    @Binding var closeViewTask: Bool
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                //MARK: Buttons
+                HStack {
+                    Button {
+                        closeViewTask.toggle()
+                    } label: {
+                        Image(systemName: "plus")
+                            .resizable()
+                            .rotationEffect(Angle(degrees: 45))
+                            .frame(width: 20, height: 20)
+                            .foregroundColor(.black)
+                    }
+                    Spacer()
+                    Button {
+                        showSideView.toggle()
+                    } label: {
+                        Image(systemName: "gearshape.fill")
+                            .resizable()
+                            .rotationEffect(Angle(degrees: 45))
+                            .frame(width: 20, height: 20)
+                            .foregroundColor(.black)
+                    }
+                }
+                .padding()
+                
+                //MARK: Title
+                Text(title)
+                    .font(Font(Roboto.thinItalic(size: 18)))
+                
+                //MARK: Assigned to
+                VStack {
+                    HStack {
+                        Image(image)
+                            .resizable()
+                            .frame(width: 44, height: 44)
+                            .clipShape(Circle())
+                            .padding()
+                        VStack(alignment: .leading) {
+                            Text("Assigned to")
+                                .font(Font(Roboto.regular(size: 16)))
+                                .foregroundColor(.secondary)
+                                .padding(.bottom, 01)
+                            Text(username)
+                                .font(Font(Roboto.thinItalic(size: 16)))
+                        }
+                        Spacer()
+                    }
+                    Divider()
+                        .frame(width: 295)
+                        .padding(.top, -10)
+                }
+                .padding(.vertical, -10)
+                
+                //MARK: Due Date
+                VStack {
+                    HStack {
+                        Image(systemName: "calendar")
+                            .foregroundColor(.secondary)
+                            .frame(width: 44, height: 44)
+                            .offset(y: -14)
+                            .padding()
+                        VStack(alignment: .leading) {
+                            Text("Due Date")
+                                .font(Font(Roboto.regular(size: 16)))
+                                .foregroundColor(.secondary)
+                                .padding(.bottom, 1)
+                            Text(dueDate)
+                                .font(Font(Roboto.thinItalic(size: 16)))
+                        }
+                        Spacer()
+                    }
+                    Divider()
+                        .frame(width: 295)
+                        .padding(.vertical, -10)
+                }
+                .padding(.top, -10)
+                
+                //MARK: Description
+                VStack {
+                    HStack {
+                        Image(systemName: "doc.plaintext")
+                            .foregroundColor(.secondary)
+                            .frame(width: 44, height: 44)
+                            .offset(y: -22)
+                            .padding()
+                        VStack(alignment: .leading) {
+                            Text("Description")
+                                .font(Font(Roboto.regular(size: 16)))
+                                .foregroundColor(.secondary)
+                                .padding(.bottom, 1)
+                            Text(description)
+                                .font(Font(Roboto.regular(size: 16)))
+                                .lineLimit(2)
+                        }
+                        Spacer()
+                    }
+                    Divider()
+                        .frame(width: 295)
+                        .padding(.vertical, -10)
+                }
+                .padding(.top, -10)
+                
+                //MARK: Members
+                VStack {
+                    HStack {
+                        Image(systemName: "person.3")
+                            .foregroundColor(.secondary)
+                            .frame(width: 44, height: 44)
+                            .offset(y: -22)
+                            .padding()
+                        VStack(alignment: .leading) {
+                            Text("Members")
+                                .font(Font(Roboto.regular(size: 16)))
+                                .foregroundColor(.secondary)
+                                .padding(.bottom, 1)
+                            HStack {
+                                Image(image)
+                                    .resizable()
+                                    .frame(width: 32, height: 32)
+                                    .clipShape(Circle())
+                                Image(image)
+                                    .resizable()
+                                    .frame(width: 32, height: 32)
+                                    .clipShape(Circle())
+                            }
+                        }
+                        Spacer()
+                    }
+                    Divider()
+                        .frame(width: 295)
+                        .padding(.vertical, -10)
+                }
+                .padding(.top, -10)
+                
+                //MARK: Tag
+                HStack {
+                    Image(systemName: "link")
+                        .foregroundColor(.secondary)
+                        .frame(width: 44, height: 44)
+                        .offset(y: -22)
+                        .padding()
+                    VStack(alignment: .leading) {
+                        Text("Tag")
+                            .font(Font(Roboto.regular(size: 16)))
+                            .foregroundColor(.secondary)
+                            .padding(.bottom, 1)
+                        Text(tag)
+                            .frame(width: 90, height: 40)
+                            .overlay(RoundedRectangle(cornerRadius: Constants.radiusFive)
+                                .stroke(lineWidth: 1)
+                                .foregroundColor(.customGray))
+                            .foregroundColor(color)
+                        
+                    }
+                    Spacer()
+                }
+                //MARK: Show Comments
+                if showComments {
+                    CommentsView()
+                        .padding(.vertical)
+                }
+
+                CustomBlueFilledButton(text: "Complete Task") {
+                    
+                }
+                .padding(.top, 10)
+                
+                //MARK: Comment Button
+                Button {
+                    withAnimation {
+                        showComments.toggle()
+                    }
+                } label: {
+                    HStack {
+                        Text("Comment")
+                            .font(Font(Roboto.thinItalic(size: 17)))
+                        VStack {
+                            Image(systemName: "chevron.down")
+                                .offset(y: 4)
+                                .foregroundColor(Color.customGray)
+                            Image(systemName: "chevron.down")
+                                .foregroundColor(.black)
+                        }
+                        .rotationEffect(Angle(degrees: showComments ? -180 : 0))
+                    }
+                }
+                .padding()
+                .foregroundColor(.secondary)
+                
+                
+            }
+            
+        }
+        .frame(width: 343, height: 716)
+        .background(.white)
+        .cornerRadius(Constants.radiusFive)
+
+    }
+}
+
+struct ViewTask_Previews: PreviewProvider {
+    @State static var showSideView = false
+    @State static var closeViewTask = false
+    static var previews: some View {
+        ViewTask(showSideView: $showSideView, closeViewTask: $closeViewTask)
+    }
+}
